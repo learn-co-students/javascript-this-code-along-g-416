c79d9f68e57ed2d183ea20089804d8ae3773da64
michaelsimon <michael.simon@gmail.com>
2017-03-08 21:34:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const app = "I don't do much."
+//create an Object
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles"
+}
+
+//create a constructor to create sandwich objects
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+}
+
+//plus create an object with a function on it.
+var pbj = {
+  bread: "white",
+  ingredients: ["peanut butter", "jelly"],
+  cut: "triangles",
+  name: "peanut butter and jelly",
+  serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
+}
+
+//if we want to make this function a constructor we can do
+function Sandwich(bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+  this.serve = function() {
+    console.log("here's your" + this.name + ", enjoy!")
+  }
+}
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"], "rectangle");
+
+blt.serve()
+
+var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
+// add name to the blt before serving
+blt.name = "BLT";
+blt.serve();
