15f3ace63d812d3d26b4987a3616d85386daf10c
github username <github email address>
2021-02-23 19:27:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 const app = "I don't do much."
+var pbj={
+    bread: "white",
+    ingredients: ["peanut butter","jelly"],
+    cut: "triangles"
+}
+
+function Sandwich(bread, ingredients, cut) {
+    this.bread = bread;
+    this.ingredients = ingredients;
+    this.cut = cut;
+    this.serve = function() {
+        console.log("here's your " + this.name + ", enjoy!");
+      }
+  }
+
+
+  var pbj = {
+    bread: "white",
+    ingredients: ["peanut butter", "jelly"],
+    cut: "triangles",
+    name: "peanut butter and jelly",
+    serve: function() {
+      console.log("here's your " + this.name + ", enjoy!");
+    }
+  }
+
+  pbj.serve()
